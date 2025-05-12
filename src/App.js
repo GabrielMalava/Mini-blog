d4c3194c82a78d053243732fbddc88aef55db377
GabrielMalava <gabrielmalavazi_dev@outlook.com>
2025-05-12 16:45:21 -0300

    aula131

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 // paages
 
@@ -9,10 +11,14 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
