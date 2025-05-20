f70f965ce40964c580657cf20b18a68855efbb9e
GabrielMalava <gabrielmalavazi_dev@outlook.com>
2025-05-20 17:13:56 -0300

    correções

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -49,8 +49,7 @@ export const useAuthentication = () => {
       console.log(error.message);
       console.log(typeof error.message);
       let systemErrorMessage;
-
-      if (error.massage.includes('Password')) {
+      if (error.message.includes('Password')) {
         systemErrorMessage = 'A senha precisa conter pelo menos 6 caracteres.';
       } else if (error.message.includes('email-already')) {
         systemErrorMessage = 'E-mail já cadastrado.';
@@ -72,7 +71,29 @@ export const useAuthentication = () => {
   }, []);
 
   //login - sing in
-  const login = async () => {};
+  const login = async (data) => {
+    checkIfIsCancelled();
+
+    setLoading(true);
+    setError(false);
+
+    try {
+      await signInWithEmailAndPassword(auth, data.email, data.password);
+      setLoading(false);
+    } catch (error) {
+      let systemErrorMessage;
+
+      if (error.message.includes('user-not-found')) {
+        systemErrorMessage = 'Usuário não encontrado';
+      } else if (error.message.includes('wrong-password')) {
+        systemErrorMessage = 'Senha incorreta.';
+      } else {
+        systemErrorMessage = 'Ocorreu um erro, por favor tente mais tarde';
+      }
+      setError(systemErrorMessage);
+      setLoading(false);
+    }
+  };
 
   return {
     auth,
@@ -80,5 +101,6 @@ export const useAuthentication = () => {
     error,
     loading,
     logout,
+    login,
   };
 };
